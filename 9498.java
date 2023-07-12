8517aa3281785e4a64209d7a2bd01aae3dc4d80d
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-12 11:43:06 +0300

    Create 9498.java

---
+++ 9498.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main{
+
+        //9498
+
+        public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		int grade = Integer.parseInt(br.readLine());
+        if(grade>=90 && grade <=100){
+            System.out.println("A");
+        }
+        else if(grade>=80 && grade <=89){
+            System.out.println("B");
+        }
+        else if(grade>=70 && grade <=79){
+            System.out.println("C");
+        }
+        else if(grade >=60 && grade <=69){
+            System.out.println("D");            
+        }
+        else
+            System.out.println("F");
+    }
+}
