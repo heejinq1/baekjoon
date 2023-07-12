12416726c980aa8a482b5d27f2e388ef37200280
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-12 11:16:08 +0300

    Create 10171.java

---
+++ 10171.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main{
+
+        //10171
+
+        public static void main(String[] args) throws IOException {
+		
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
