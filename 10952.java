b7e72a89c4ea764974c7a314f1c292df0763bd8d
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-12 12:55:51 +0300

    Create 10952.java

---
+++ 10952.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+
+  \\ 10952
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int x,y;
+        x= sc.nextInt();
+        y= sc.nextInt();
+        while(x!=0 && y!=0){
+            System.out.println(x+y);
+             x= sc.nextInt();
+             y= sc.nextInt();
+            
+        }
+    }
+}
