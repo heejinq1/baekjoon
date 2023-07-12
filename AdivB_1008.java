63f57ea1a3fe5e811c3bd7181a1c0f9d66b4554e
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-12 10:45:39 +0300

    Create AdivB_1008.java

---
+++ AdivB_1008.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+
+    //A/B_1008
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.print((double)a/b);
+    }
+}
