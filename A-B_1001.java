61067c7bd08dc26ee2479ef68f511ff96a484dde
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-12 10:35:05 +0300

    Create A-B_1001.java

---
+++ A-B_1001.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+
+    //A-B_1001
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a-b);
+    }
+}
