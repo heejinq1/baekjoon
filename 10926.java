83d3ad636bc10d03be77c20e3eda55c7c31d617f
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-12 10:50:09 +0300

    Create 10926.java

---
+++ 10926.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+
+    //10926
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String username = sc.nextLine();
+        System.out.println(username+"??!");
+    }
+}
