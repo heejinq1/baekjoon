960708eda6541c34465da3988b8380ad9614a4dc
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 10:46:36 +0300

    Create 16394.java

---
+++ 16394.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 16394
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n-1946);
+        
+	}
+}
