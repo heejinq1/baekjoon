754097bc40d5247ef5c664077c3db11e114081d5
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 10:18:15 +0300

    Create 14581.java

---
+++ 14581.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 14581
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+name+"::fan:");
+        System.out.println(":fan::fan::fan:");
+
+        
+
+	}
+}
