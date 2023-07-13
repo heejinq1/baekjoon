6517bb331ceb75506001ae85fb45aff896174a97
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 05:06:02 +0300

    Create 2743.java

---
+++ 2743.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    // 2743
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String word = br.readLine();
+        
+        System.out.println(word.length());
+	}
+}
