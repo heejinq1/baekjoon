fd5c0e7c30028fe16dbb9576b6f0ecf7da60183f
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 10:24:25 +0300

    Create 15680.java

---
+++ 15680.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 15680
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        if(n==0)
+            System.out.println("YONSEI");
+        else
+            System.out.println("Leading the Way to the Future");
+
+        
+
+	}
+}
