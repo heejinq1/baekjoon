69a606376185d003389ebc0971fc3f622942b417
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 09:56:31 +0300

    Create 2338.java

---
+++ 2338.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 2338
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc= new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+
+        System.out.println(a.add(b));
+        System.out.println(a.subtract(b));
+        System.out.println(a.multiply(b));
+        
+
+        
+
+        
+	}
+}
