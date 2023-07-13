f129dc37b539b875594e3952bdc580b3bac2b794
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 10:28:48 +0300

    Create 27327.java

---
+++ 27327.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 27327
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        System.out.println(day*24);
+	}
+}
