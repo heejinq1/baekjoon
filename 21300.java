80f3d55a6d9706544505a8c7218b766f4bd17740
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 11:36:27 +0300

    Create 21300.java

---
+++ 21300.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 21300
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+        System.out.println((a+b+c+d+e+f)*5);
+	}
+}
