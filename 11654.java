6036043e854afb8380b4957bd6c832790a1c6a6b
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 05:11:48 +0300

    Create 11654.java

---
+++ 11654.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    // 11654
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String temp = br.readLine();
+        System.out.print(temp.charAt(0)+0);
+        
+	}
+}
