10b765a0d78c074d65f80d86db46f5a641ff00b7
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 10:16:04 +0300

    Create 13277.java

---
+++ 13277.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 13277
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        BigInteger a= sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.multiply(b));
+        
+
+	}
+}
