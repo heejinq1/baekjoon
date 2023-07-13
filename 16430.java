ca1e5df2fb749d5c39ca925edf4a3709e2ffbe73
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 10:51:26 +0300

    Create 16430.java

---
+++ 16430.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 16430
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println((b-a)+" "+b);
+        
+	}
+}
