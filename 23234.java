607c1fe3b1a38ad8e338b0c8d36a2b9063eda45b
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 10:26:52 +0300

    Create 23234.java

---
+++ 23234.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 23234
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("The world says hello!");
+	}
+}
