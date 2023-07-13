0652c2c9d2b1235a8a4aa40cc6f9d73e3fb52076
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 10:31:51 +0300

    Create 27331.java

---
+++ 27331.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 27331
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        System.out.println(a+b);
+	}
+}
