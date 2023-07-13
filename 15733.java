6f92d53df019aafbd6038dfbe6c64e88dc97d102
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 10:25:26 +0300

    Create 15733.java

---
+++ 15733.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 15733
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("I'm Sexy");
+	}
+}
