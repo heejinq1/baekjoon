08f69d88dd8bd77013dd704282bba861dcc34c51
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 10:16:22 +0300

    Create 14645.java

---
+++ 14645.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 14645
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b = sc.nextInt();
+        for(int i = 0; i < a; i++){
+            String temp = br.readLine();
+        }
+        System.out.println("비와이");
+        
+
+	}
+}
