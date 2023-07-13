333eda1c1f49e68df06de2a8e6edbc4b4eba5d09
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 10:04:25 +0300

    Create 2558.java

---
+++ 2558.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 2558
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        System.out.println(a+b);
+        
+
+	}
+}
