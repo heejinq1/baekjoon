241af5ae63789c9423568dc8bb21e618af97ce62
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 10:34:05 +0300

    Create 24078.java

---
+++ 24078.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 24078
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a%21);
+	}
+}
