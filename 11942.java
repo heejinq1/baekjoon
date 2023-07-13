8efaf1725c39e940df9eecb7bd4e590a3f088c41
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-13 10:11:21 +0300

    Create 11942.java

---
+++ 11942.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 11942
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("고려대학교");
+        
+
+	}
+}
