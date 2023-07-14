4f43a8d7804dc1da68aca6a58f942e7c737b687f
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-14 18:37:47 +0300

    Create 9653.java

---
+++ 9653.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 9653
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("    8888888888  888    88888");
+        System.out.println("   88     88   88 88   88  88");
+        System.out.println("    8888  88  88   88  88888");
+        System.out.println("       88 88 888888888 88   88");
+        System.out.println("88888888  88 88     88 88    888888");
+        System.out.println("");
+        System.out.println("88  88  88   888    88888    888888");
+        System.out.println("88  88  88  88 88   88  88  88");
+        System.out.println("88 8888 88 88   88  88888    8888");
+        System.out.println(" 888  888 888888888 88  88      88");
+        System.out.println("  88  88  88     88 88   88888888");
+	}
+}
