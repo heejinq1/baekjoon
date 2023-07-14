fe3fe56a82a82ae07c111eec7cdf0296148a4c3d
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-14 19:10:32 +0300

    Create 15000.java

---
+++ 15000.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Scanner;
+public class Main {
+
+    // 15000
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        Scanner sc= new Scanner(System.in);
+        String temp = sc.nextLine();
+        System.out.println(temp.toUpperCase());
+        
+       
+    }
+}
