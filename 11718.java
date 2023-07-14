d119218a50f393324c85b6673af66c9ad27f3d83
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-14 12:51:19 +0300

    Create 11718.java

---
+++ 11718.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+
+    // 11718
+    public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
+
+	}
+}
