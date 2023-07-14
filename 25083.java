911edcb7e756fb7b7c237d7e36762a2892cbee99
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-14 12:53:00 +0300

    Create 25083.java

---
+++ 25083.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+
+    // 25083
+    public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //Scanner sc = new Scanner(System.in);
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+        
+
+	}
+}
