31c6701b002b27af6ba6a78e535fcfcc9b3e97a4
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-14 13:52:18 +0300

    Create 2393.java

---
+++ 2393.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    // 2393
+    public static void main(String args[]) throws IOException {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("  ___  ___  ___");
+        System.out.println("  | |__| |__| |");
+        System.out.println("  |           |");
+        System.out.println("   \\_________/");
+        System.out.println("    \\_______/");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |_____|");
+        System.out.println("  __/       \\__");
+        System.out.println(" /             \\");
+        System.out.println("/_______________\\");
+
+    }
+}
