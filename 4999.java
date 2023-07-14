e90d9b5ca5904b911be04771dd40da350b9b071d
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-14 10:30:40 +0300

    Create 4999.java

---
+++ 4999.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 4999
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String first = br.readLine();
+        String second = br.readLine();
+        if(first.length()>= second.length()){
+            System.out.println("go");
+        }
+        else    
+            System.out.println("no");
+        
+        
+	}
+}
