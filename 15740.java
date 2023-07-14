c3dc1aae96f9d0658194f29a48fb25d1321ec792
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-14 07:15:54 +0300

    Create 15740.java

---
+++ 15740.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 15740
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc= new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.add(b));
+        
+        
+        
+	}
+}
