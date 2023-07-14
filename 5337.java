2587a8d145bc9557d5e45c77f57f7bd2084a4b27
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-14 07:04:57 +0300

    Create 5337.java

---
+++ 5337.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 5337
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+
+        
+        
+        
+	}
+}
