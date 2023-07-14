f1d421ea9841ea4dfcef74be1a7ea0eff44f7e19
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-14 12:39:57 +0300

    Create 7287.java

---
+++ 7287.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+
+    // 7287
+    public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("110");
+        System.out.println("heejinq1");
+
+	}
+}
