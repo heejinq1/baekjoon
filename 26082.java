90719fb0c7169a55f23201a7d40044fc61384ea8
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-14 07:23:17 +0300

    Create 26082.java

---
+++ 26082.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 26082
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c= sc.nextInt();
+        int result = 0;
+        System.out.println(((c*b)/a)*3);
+        
+        
+        
+	}
+}
