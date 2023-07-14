21f9b7cbab5ca302c898868354ab936a92cd7c21
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-14 11:30:56 +0300

    Create 15963.java

---
+++ 15963.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+public class Main {
+
+    // 15963
+	public static void main(String args[]) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc= new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        if(a==b){
+            System.out.println("1");
+        }
+        else
+            System.out.println("0");
+        
+	}
+}
+
