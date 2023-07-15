fac3789199ff94cfdbe773672b0facfd7ba32c41
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-15 10:42:42 +0300

    Create 3046.java

---
+++ 3046.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Main {
+
+    // 3046
+    public static void main(String args[]) throws IOException {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        Scanner sc= new Scanner(System.in);
+
+        int r1= sc.nextInt();
+        int s = sc.nextInt();
+        System.out.println(((s*2)-r1));
+        
+
+    }
+}
