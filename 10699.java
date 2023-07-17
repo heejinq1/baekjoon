62021acab37cee7b17d13f7eb1ff57fddeed61dd
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-17 14:11:33 +0300

    Create 10699.java

---
+++ 10699.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Main {
+
+    // 10699
+    public static void main(String args[]) throws IOException {
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date d = new Date();
+        System.out.println(format.format(d));
+
+    }
+}
