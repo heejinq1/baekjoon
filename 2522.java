8d09696cec21b20be02671f2dbfb8fc8179de274
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-22 21:05:10 +0300

    Create 2522.java

---
+++ 2522.java
@@ -0,0 +1,28 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+    // 2522
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+        String star = "*";
+        String space = "";
+        for(int j = 0; j<i-1 ; j++){
+            space+=" ";
+        }
+        for(int j = 0; j< i-1; j++){
+            System.out.println(space+star);
+            star+="*";
+            space=space.substring(1);
+        }
+        System.out.println(star);
+        for(int k=0; k<i-1; k++){
+            star=star.substring(1);
+            space+=" ";
+            System.out.println(space+star);
+        }
+	}
+}
