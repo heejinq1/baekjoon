4b6f7063bdfba2842044698084aee087a4fbaea9
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2023-07-24 20:25:04 +0300

    Create 2747.java

---
+++ 2747.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    // 2747
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		//StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[46];  
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.print(dp[n]);
+    }
+	
+}
